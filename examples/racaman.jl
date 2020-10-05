b4a22dfe111376a0160915f125f5bcdabb0ea89d
Deyan Dyankov <deyan@dyankov.name>
2020-10-05 23:57:33 +0100

    Scaler is now a parameter

--- examples/racaman.jl
+++ examples/racaman.jl
@@ -9,12 +9,12 @@ end
 function frame(scene, framenumber)
     max_iteration = maximum(scene.movie.movieframerange)
 
+    scaler = scene.opts[:scaler]
     sequence = scene.opts[:sequence]
     sequence_so_far = sequence[1:framenumber]
     points = [Point(x, 0) for x in sequence_so_far]
 
     rotate(-π/4)
-    scaler = 10
 
     sethue(1, 1 - framenumber / max_iteration, 0)
 
@@ -30,25 +30,23 @@ function frame(scene, framenumber)
         left += 1
         right += 1
     end
-
-    # draw a line..
-    sethue("white")
-    line_start = points[1]
-    line_end = Point(maximum(sequence_so_far) * scaler, 0)
-    
-    line(line_start, line_end, :stroke)
 end
 
 function animate_racaman(N)
     racaman_movie = Movie(1024, 1024, "racaman", 1:N)
-    optarg = Dict(:sequence => racaman(N))
+    optarg = Dict(
+        :sequence => racaman(N),
+        :scaler => 10
+    )
 
-    scenes_backdrop = Scene(racaman_movie, backdrop, optarg=optarg)
-    scenes_frame = Scene(racaman_movie, frame, optarg=optarg)
-    
+    scenes = [
+        Scene(racaman_movie, backdrop, optarg=optarg),
+        Scene(racaman_movie, frame, optarg=optarg),
+    ]
+
     animate(
         racaman_movie,
-        [scenes_backdrop, scenes_frame],
+        scenes,
         creategif=true,
         framerate=5,
         pathname="/tmp/animationtest.gif"
